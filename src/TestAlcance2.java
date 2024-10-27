79f398f6ab41cbf85f5ebdddeca515c2cde50bc4
Luisda <luisda915@gmail.com>
2024-10-27 17:23:13 -0300

    se optimiza el codigo

--- src/TestAlcance2.java
+++ src/TestAlcance2.java
@@ -1,23 +1,26 @@
+import java.util.Scanner;
 
 public class TestAlcance2 {
 
 	public static void main (String [] args) {
         System.out.println ("pruebas condicionales");
+Scanner Sc = new Scanner(System.in);
+System.out.println("ingrese su edad, por favor.");
+        int edad = Sc.nextInt();
+        System.out.println("Ingrese cuantas personas son:");
+        int cantidadPersonas = Sc.nextInt();
+        boolean acompañado = cantidadPersonas >= 2;// por quitar if innecesarios. se pone aqui. de la otra forma funcion pero es mucho codigo.
 
-        int edad = 20;
-        int cantidadPersonas = 3;
-        boolean acompañado;
-
-        if (cantidadPersonas >= 2) {
-            acompañado = true;
-        } else {
-            acompañado = false;
-        }
+ //       if (cantidadPersonas >= 2) {
+   //         acompañado = true;
+     //   } else {
+       //     acompañado = false;
+        //}
 
         if (edad >= 18 || acompañado) {
             System.out.println ("Bienvenido");
         } else {
             System.out.println ("desafortunadamente no puedes ingresar");
         }
-    }
+ Sc.close();   }
 }
