86c773576ddca66234243203ab983c6d4f3fc99d
Luisda <luisda915@gmail.com>
2024-10-27 16:29:12 -0300

    Se cambia a else. por no haber mas condiciones.

--- src/test_condiones.java
+++ src/test_condiones.java
@@ -5,24 +5,22 @@ public class test_condiones {
 
     public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
+System.out.println("ingrese el salario estimados");
         double salario = sc.nextDouble();
 
-        //if(salario < 2600.0)  
-           // System.out.println("Su tasa es del  15%");
-            //System.out.println("Puede deducir hasta R$ 350");
-        
-       // if(salario <= 3750.0)  
-         //   System.out.println("Su tasa es del  22,5%");
-           // System.out.println("Puede deducir hasta R$ 636");
         if (salario < 2600.0) {
             System.out.println ("Su tasa es del 15%");
             System.out.println ("Puede deducir R $ 350");
         }
 
-        if (salario < 3750.0) {
+        else {
             System.out.println ("Su tasa es del 22.5%");
             System.out.println ("Puede deducir R $ 636");
         }
+        // if(salario <= 3750.0)  
+        //   System.out.println("Su tasa es del  22,5%");
+          // System.out.println("Puede deducir hasta R$ 636");
+
     sc.close();
     
     }    
